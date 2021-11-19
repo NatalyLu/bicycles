805f4bccf85c94d533324d7fe2d890b5172648d4
Natalia <lukashina_natalia@mil.ru>
2021-11-19 06:53:40 +0300

    form validation

--- source/js/_2_consts.js
+++ source/js/_2_consts.js
@@ -1,7 +1,7 @@
 (function () {
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
-  var FORM_ACTION = "/action";
+  var FORM_ACTION = "https://echo.htmlacademy.ru/";
 
   window.util = {
     ESC_KEYCODE: ESC_KEYCODE,
